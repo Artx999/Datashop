48887c80574146b3a7b0a6ff3820399e23dffbbd
Artx999 <54852665+Artx999@users.noreply.github.com>
2021-04-09 14:22:16 +0200

    Progress with design
    Finished and exported logo, added potential header images, added color palette and other documentation.
    Also added some of the basic structure from another project (will change later).

---
+++ structure/head.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="no">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="<?php echo $root ?>style.css">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.6/css/line.css">
+    <script src="<?php echo $root ?>script.js"></script>
+</head>
---
+++ structure/nav.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+        <li><a href="<?php print $root . "index.php" ?>">Hjem</a></li>
+        <li><a href="<?php print $root . "lift-pass.php" ?>">Heiskort</a></li>
+        <li><a href="<?php print $root . "cabins.php" ?>">Hytter</a></li>
+    </ul>
+</nav>
